3334b0bab70905ee281a4fbb07d227754e6b1006
imadhahmed <imadhahmeth08@gmail.com>
2024-03-08 18:37:13 +0530

    ch

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,16 +2,33 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
+
 import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
 
 function Projects() {
   return (
-   0
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={chatify}
+              isBlog={false}
+              title="Portfolio"
+              description="I create my portfolio using react js,javascript and bootstrap."
+              demoLink="https://imadhahmed.github.io/portfolio/"
+            />
+          </Col>
+
+         
+        </Row>
+      </Container>
+    </Container>
   );
 }
 
