cf39a3ccfd43f22b9082a5cf257f50548df42188
imadhahmed <imadhahmeth08@gmail.com>
2024-11-25 10:27:39 +0530

    project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,6 +4,7 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 
 import chatify from "../../Assets/Projects/chatify.png";
+import organizer from "../../Assets/Projects/organizer.png";
 
 function Projects() {
   return (
@@ -22,6 +23,14 @@ function Projects() {
               title="Portfolio"
               description="I create my portfolio using react js,javascript and bootstrap."
               demoLink="https://imadhahmed.github.io/portfolio/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={organizer}
+              isBlog={false}
+              title="Personal Organizer"
+              description="I create Personal Organizer Application  using C++,.NET Framework."
             />
           </Col>
 
