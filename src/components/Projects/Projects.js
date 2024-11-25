4e960be93d82fbe7ede95080a9cc43d28e0f1571
imadhahmed <imadhahmeth08@gmail.com>
2024-11-25 18:48:48 +0530

    erl

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -14,7 +14,6 @@ function Projects() {
         <h1 className="project-heading">
           My Recent <strong className="purple">Works </strong>
         </h1>
-        
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
@@ -31,10 +30,10 @@ function Projects() {
               isBlog={false}
               title="Personal Organizer"
               description="I create Personal Organizer Application  using C++,.NET Framework."
+              demoLink="https://github.com/imadhahmed/personalOrganizer.git"
             />
           </Col>
 
-         
         </Row>
       </Container>
     </Container>
