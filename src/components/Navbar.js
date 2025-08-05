11f83062876ce488fd2226fcc437fb3bb26d202f
imadhahmed <imadhahmeth08@gmail.com>
2025-08-05 22:16:11 +0530

    cha

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -88,7 +88,7 @@ function NavBar() {
                 to="/resume"
                 onClick={() => updateExpanded(false)}
               >
-                <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
+                <CgFileDocument style={{ marginBottom: "2px" }} /> CV
               </Nav.Link>
             </Nav.Item>
 
